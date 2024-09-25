23b116d192cc84b62eb79485793c97a50c59fa90
root <gharibehhamzh@gemail.com>
2024-09-25 09:16:14 +0300

    Exercise

--- script.js
+++ script.js
@@ -81,3 +81,22 @@ function generateEmail(name, dowain) {
 return name + '@' + dowain 
 }
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com")); 
+
+console.log('---------------------------------------------');
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+function greetUser( name, timeOfDay) {
+  return `good ${timeOfDay}, ${name}!`;
+}
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
