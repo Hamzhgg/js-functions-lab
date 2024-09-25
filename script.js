d32305379ba89cc28dbd0f9cae2277e66aecd8d7
root <gharibehhamzh@gemail.com>
2024-09-25 10:56:34 +0300

    Exercise

--- script.js
+++ script.js
@@ -100,3 +100,41 @@ function greetUser( name, timeOfDay) {
   return `good ${timeOfDay}, ${name}!`;
 }
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+console.log('---------------------------------------------');
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+
+function maxOfThree(a, b, c) {
+  return Math.max(a, b, c);
+}
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+
+console.log('---------------------------------------------');
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+function calculateTip() {
+
+}
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+console.log('---------------------------------------------');
